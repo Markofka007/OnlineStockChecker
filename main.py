cb827c29c29fa79611c2eb78d0650eba4ff4f22f
Mark <69377981+TheBigCarrot@users.noreply.github.com>
2020-12-07 21:31:33 -0800

    OnlineStockChecker v2.1
    I am calling this version 2 because I already completed a working version last night. The only thing that was added in version 2 was support for email clients to notify the user when a product comes back in stock via email. I am currently trying to set the subject of the email as the product name, then I think this will be 100% complete.
    
    When I am completely done, I will make a separate version of this program without the mail client and with a lot more types of graphic cards across more websites, then I will livestream the live updates.

---
+++ main.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from datetime import datetime
+from termcolor import colored
+import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
+# mail client stuff
+sender_email = ''  # SENDER EMAIL
+rec_email = ''  # RECEIVER EMAIL
+password = ''  # SENDER EMAIL PASSWORD
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.ehlo()
+server.login(sender_email, password)
+
+
+def send_email(product_name, product_url):
+    mail_subject = f'{product_name}'
+    mail_body = f'{product_url}'
+    msg = f'Subject: {mail_subject}\n\n{mail_body}'
+    server.sendmail(sender_email, rec_email, msg)
+
+# google chrome driver (user agent)
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+# url list
+bestbuy_3080 = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440'
+bestbuy_3070 = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442'
+url_list = [bestbuy_3080, bestbuy_3070]
+array_pointer = 0
+
+print(colored(f'Now Testing [{len(url_list)}] Sites', 'yellow'))
+
+while True:
+    array_pointer = 0
+    for url in url_list:
+        driver.get(url)
+        array_pointer += 1
+        # print(driver.title)
+        time.sleep(1)
+        if "Sold Out" in driver.page_source:
+            print(f"{datetime.now()} <PST>: {driver.title}: {colored('Currently Sold Out', 'red')}")
+            send_email(f'{url_list[array_pointer - 1]} sold out :(', url)
+        else:
+            print(f"{datetime.now()} <PST>: {driver.title}: {colored('CURRENTLY IN STOCK', 'green')}")
